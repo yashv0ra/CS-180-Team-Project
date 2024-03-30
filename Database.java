a3bfe36534ffd35196d91d6807a5b9e792498f0a
ZackWang0365 <164405671+ZackWang0365@users.noreply.github.com>
2024-03-29 20:55:13 -0400

    Add files via upload
    added methods to database; added a exception to handle errors of modify user. what method should user class should implement.

---
+++ Database.java
@@ -0,0 +1,115 @@
+import java.util.ArrayList;
+
+public class Database {
+    private ArrayList<User> listOfUsers;
+
+    public boolean login(String email, int password) {
+        for (int i = 0; i < listOfUsers.size(); i++) {
+            if (listOfUsers.get(i).getEmail().equals(email)) {
+                if (listOfUsers.get(i).getPassword() == password) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    //for the major search, I am thinking we should do a scroll down menu on the GUI part instead of
+    //letting the user freely input any word they want, in order to reduce the complexity of this program
+    public ArrayList<User> usersMajorSearch (String major) {
+        ArrayList<User> listThatMatches = new ArrayList<>();
+        for (int i = 0; i < listOfUsers.size(); i++) {
+            if (listOfUsers.get(i).getMajor().equals(major)) {
+                listThatMatches.add(listOfUsers.get(i));
+            }
+        }
+        if (listOfUsers.size() > 0) {
+            return listThatMatches;
+        } else {
+            return null;
+        }
+    }
+
+    //name should be in the form of first and last name's first letter cap and the rest is small case,
+    //with a space between the first and last name.
+    //"name" that the user want to search have to be from the first letter of the first or last name
+    //for example, for search Zack Wang, the string can be "Za" or "wan"
+    public ArrayList<User> usersNameSearch (String name) {
+        ArrayList<User> listThatMatches = new ArrayList<>();
+
+        if (name.length() > 1) {
+            String toForm = name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
+        }
+
+        for (int i = 0; i < listOfUsers.size(); i++) {
+            if (listOfUsers.get(i).getName().contains(name)
+                    || listOfUsers.get(i).getName().contains(toString())) {
+                listThatMatches.add(listOfUsers.get(i));
+            }
+        }
+        if (listOfUsers.size() > 0) {
+            return listThatMatches;
+        } else {
+            return null;
+        }
+    }
+
+    public User usersEmailSearch (String email) {
+        User returnUser = null;
+
+        if (email.contains("@")) {
+            for (int i = 0; i < listOfUsers.size(); i++) {
+                if (email.equals(listOfUsers.get(i))) {
+                    returnUser = listOfUsers.get(i);
+                }
+            }
+        } else {
+            String emailFull = email + "@purdue.edu";
+            for (int i = 0; i < listOfUsers.size(); i++) {
+                if (emailFull.equals(listOfUsers.get(i))) {
+                    returnUser = listOfUsers.get(i);
+                }
+            }
+        }
+        return returnUser;
+    }
+
+    public boolean modifyUser(User aimUser, User changed) {
+        for (int i = 0; i < listOfUsers.size(); i++) {
+            if (listOfUsers.get(i).compareTo(aimUser)) {
+                listOfUsers.get(i).setName(changed.getName());
+                listOfUsers.get(i).setMajor(changed.getMajor());
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean modifyUser(ArrayList<User> usersNeedToBeChanged, ArrayList<User> changed) throws ImpossibleChangeException{
+        if (usersNeedToBeChanged.size() != changed.size()) {
+            throw new ImpossibleChangeException("lists do not match");
+        } else {
+            for (int i = 0; i < usersNeedToBeChanged.size(); i++) {
+                modifyUser(usersNeedToBeChanged.get(i), changed.get(i));
+            }
+        }
+        return true;
+    }
+
+    //should this method the same or similar to the toString method?
+//    public String toString(int index) {
+//        String result = "name: " + listOfUsers.get(index).getName()
+//                + "\nEmail: " + listOfUsers.get(index).getEmail()
+//                + "\nMajor: " + listOfUsers.get(index).getMajor();
+//        return result;
+//    }
+
+    //get the user index
+//    public int userView(User user) {
+//        for (int i = 0; i < listOfUsers.size(); i++) {
+//            if ()
+//        }
+//    }
+
+
+}
---
+++ UserInterface.java
@@ -0,0 +1,14 @@
+public interface UserInterface {
+    public String getName();
+    public int getPassword();
+    public String getEmail();
+    public String getMajor();
+    public void setName(String name);
+    public void setPassword(int password);
+    public void setMajor(String major);
+    public boolean compareTo(User user);
+    //only check email
+    public boolean modifyUser(User user);
+    public String toString();
+
+}
