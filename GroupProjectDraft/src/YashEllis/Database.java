c2c17b7e966827125d389b739ac1187be86f7497
yashv0ra <136860838+yashv0ra@users.noreply.github.com>
2024-04-01 16:04:42 -0400

    Update Database.java

--- GroupProjectDraft/src/YashEllis/Database.java
+++ GroupProjectDraft/src/YashEllis/Database.java
@@ -1,5 +1,12 @@
 import java.util.ArrayList;
-
+/**
+ * The central database for all of the users.
+ *
+ * Purdue University -- CS18000 -- Spring 2024 -- Team Project Phase 1
+ *
+ * @author Yash Vora, Ellis Sioukas, Zack Wang, Jad Karaki
+ * @version April 1, 2024
+ */
 public class Database {
     private ArrayList<User> listOfUsers;
 
@@ -10,9 +17,7 @@ public class Database {
     public boolean login(String email, String password) {
         for (int i = 0; i < listOfUsers.size(); i++) {
             if (listOfUsers.get(i).getEmail().equals(email)) {
-                if (listOfUsers.get(i).getPassword().equals(password)) {
-                    return true;
-                }
+                return listOfUsers.get(i).getPassword().equals(password);
             }
         }
         return false;
@@ -20,7 +25,7 @@ public class Database {
 
     //for the major search, I am thinking we should do a scroll down menu on the GUI part instead of
     //letting the user freely input any word they want, in order to reduce the complexity of this program
-    public ArrayList<User> usersMajorSearch (String major) {
+    public ArrayList<User> usersMajorSearch(String major) {
         ArrayList<User> listThatMatches = new ArrayList<>();
         for (int i = 0; i < listOfUsers.size(); i++) {
             if (listOfUsers.get(i).getMajor().equals(major)) {
@@ -38,7 +43,7 @@ public class Database {
     //with a space between the first and last name.
     //"name" that the user want to search have to be from the first letter of the first or last name
     //for example, for search Zack Wang, the string can be "Za" or "wan"
-    public ArrayList<User> usersNameSearch (String name) {
+    public ArrayList<User> usersNameSearch(String name) {
         ArrayList<User> listThatMatches = new ArrayList<>();
 
         if (name.length() > 1) {
@@ -58,7 +63,7 @@ public class Database {
         }
     }
 
-    public User usersEmailSearch (String email) {
+    public User usersEmailSearch(String email) {
         User returnUser = null;
 
         if (email.contains("@")) {
@@ -89,7 +94,8 @@ public class Database {
         return false;
     }
 
-    public synchronized boolean modifyUser(ArrayList<User> usersNeedToBeChanged, ArrayList<User> changed) throws ImpossibleChangeException{
+    public synchronized boolean modifyUser(ArrayList<User> usersNeedToBeChanged, ArrayList<User> changed)
+            throws ImpossibleChangeException {
         if (usersNeedToBeChanged.size() != changed.size()) {
             throw new ImpossibleChangeException("lists do not match");
         } else {
@@ -99,37 +105,4 @@ public class Database {
         }
         return true;
     }
-
-    //should this method the same or similar to the toString method?
-//    public String toString(String email) {
-//        usersEmailSearch(email);
-//        String result = "name: " + listOfUsers.get(index).getName()
-//                + "\nEmail: " + listOfUsers.get(index).getEmail()
-//                + "\nMajor: " + listOfUsers.get(index).getMajor();
-//        return result;
-//    }
-
-    //get the user index
-//    public int userView(User user) {
-//        for (int i = 0; i < listOfUsers.size(); i++) {
-//            if ()
-//        }
-//    }
-
-
-//For testing this class
-    public static void main(String[] args) throws InvalidInputException {
-        User u1 = new User("personA", "1234", "personA@purdue.edu", "IE",
-                new ArrayList<>(),null,null,true);
-        User u2 = new User("personB", "1234", "personB@purdue.edu", "ECE",
-                new ArrayList<User>() {{add(u1);}},null,null,true);
-        User u3 = new User("personC", "1234", "personC@purdue.edu", "CS",
-                new ArrayList<User>() {{add(u1);add(u2);}},null,null,true);
-        User u4 = new User("personD", "2222", "personD@purdue.edu", "ME",
-                null,null,null,true);
-        Chat c = new Chat(u1, u2);
-        c.addAMessage("hello", u1);
-        Database db = new Database(new ArrayList<User>() {{add(u1);add(u2);add(u3);add(u4);}});
-    }
-
 }
