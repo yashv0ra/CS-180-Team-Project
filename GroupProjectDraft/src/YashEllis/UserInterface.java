2cecb55bf6a3f1fb0b37a9e5c866aeac3f8c6631
Webby5 <149518047+Webby5@users.noreply.github.com>
2024-03-31 21:36:00 -0400

    Update UserInterface.java

--- GroupProjectDraft/src/YashEllis/UserInterface.java
+++ GroupProjectDraft/src/YashEllis/UserInterface.java
@@ -4,11 +4,11 @@ public interface UserInterface {
     public String getEmail();
     public String getMajor();
     public void setName(String name);
-    public void setPassword(int password);
+    public void setPassword(String password);
+    public void setEmail(String email);
     public void setMajor(String major);
     public boolean compareTo(User user);
     //only check email
     public boolean modifyUser(User user);
-    public String toString();
-
+    //public String toString();
 }
