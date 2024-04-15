2471559356cf8e0f34fd144131e193d693160c0d
jkaraki4 <128740992+jkaraki4@users.noreply.github.com>
2024-04-15 15:12:59 -0400

    Update ClientThread.java

--- GroupProjectDraft/src/YashEllis/ClientThread.java
+++ GroupProjectDraft/src/YashEllis/ClientThread.java
@@ -1,25 +1,46 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ClientThread extends Thread {
-    private ServerSocket serverSocket;
     private String host;
     private int port;
     private Socket socket;
-    public ClientThread(String host, int port, ServerSocket serverSocket) {
+    public ClientThread(String host, int port) {
         this.host = host;
         this.port = port;
-        this.serverSocket = serverSocket;
     }
     public void run() {
         try {
-            socket = serverSocket.accept();
-        } catch (IOException e) {
+            //socket = serverSocket.accept();
+            try {
+                this.socket = new Socket(host, port);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            PrintWriter writer = new PrintWriter(socket.getOutputStream());
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            writer.write("Connection Established");
+            writer.flush();
+            Menu menu = new Menu();
+            menu.runClient(reader, writer);
+        } catch (Exception e) {
             return;
         }
     }
     public Socket getSocket() {
         return socket;
     }
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(new ClientThread("localhost", 4242));
+        thread.start();
+    }
+
 }
+
+
+
