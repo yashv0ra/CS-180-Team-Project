34751c506961bdb27bc884a2aae04ba2c5389d8a
jkaraki4 <128740992+jkaraki4@users.noreply.github.com>
2024-04-23 19:22:26 -0400

    Update Menu.java

--- GroupProjectDraft/src/YashEllis/Menu.java
+++ GroupProjectDraft/src/YashEllis/Menu.java
@@ -1,8 +1,10 @@
 import java.io.*;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class Menu implements Runnable{
+public class Menu implements Runnable {
     private BufferedReader reader;
     private PrintWriter writer;
 
@@ -56,7 +58,7 @@ public class Menu implements Runnable{
                 }
             }
             //Insert Welcome page with option to login or sign up and based on user choice, change account value
-            int account = 0; // 1 when user wants to log in, 2 when user wants to sign in, and 3 when user wants to exit
+            int account = 0; // 1 when user wants to log in, 2 when user wants to sign up, and 3 when user wants to exit
             boolean validUser = false; //turns true when user is logged in/ signed up
             do {
                 if (account == 1) {
@@ -74,7 +76,10 @@ public class Menu implements Runnable{
                 } else if (account == 2) {
                     emailInput = "";
                     passwordInput = "";
-                    //Let user input both
+                    String majorInput = "";
+                    String nameInput = "";
+                    String restrictionInput = ""; //"all" or "friends"
+                    //Let user input all of the above
                     if (!email.contains(emailInput)) {
                         if (emailInput.contains("@") && !emailInput.contains(" ") && !passwordInput.isEmpty()) {
                             String[] emailElements = emailInput.split("@");
@@ -82,15 +87,16 @@ public class Menu implements Runnable{
                                     && emailElements[1].equals("purdue.edu")) {
                                 email.add(emailInput);
                                 password.add(passwordInput);
+                                rewriteLoginDetails(email, password);
                                 //Synchronize the two things above
                             } else {
-                                System.out.println("Here");
                                 throw new InvalidInputException("Invalid Input!");
                             }
                         } else {
                             throw new InvalidInputException("Invalid Input!");
                         }
                         validUser = true;
+                        createUserFile(emailInput, passwordInput, majorInput, nameInput, restrictionInput);
                     } else {
                         if (email.contains(emailInput)) {
                             //throw new InvalidInputException("User with this email already exists");
@@ -99,89 +105,81 @@ public class Menu implements Runnable{
                     }
                 }
             } while (!validUser);
-        } catch (IOException e){
+        } catch (IOException e) {
             throw new RuntimeException();
         } catch (InvalidInputException r) {
         }
 
-            //User data format (each user has their own file with their respective data)
+        //User data format (each user has their own file with their respective data)
 
-            //The file names would be jkaraki@purdue.eduDataFile and would be formatted as:
-            //jkaraki@purdue.edu,1234
-            //Jad
-            //Industrial Engineering
-            //Friends:dngquan@purdue.edu,yash@purdue.edu,purduePete@purdue.edu...
-            //Blocked:cs180@purdue.edu,csdep@purdue.edu...
-            //WhoCanMessage:all
+        //The file names would be jkaraki@purdue.eduDataFile and would be formatted as:
+        //jkaraki@purdue.edu,1234
+        //Jad
+        //Industrial Engineering
+        //Friends:dngquan@purdue.edu,yash@purdue.edu,purduePete@purdue.edu...
+        //Blocked:cs180@purdue.edu,csdep@purdue.edu...
+        //WhoCanMessage:all
 
-            //here is another example:
-            //edey@purdue.edu,DoubleDouble
-            //Zach
-            //Computer Science
-            //Friends:dngquan@purdue.edu,yash@purdue.edu,purduePete@purdue.edu...
-            //Blocked:Empty
-            //WhoCanMessage:friends
-try {
-    BufferedReader bfr = new BufferedReader(new FileReader(emailInput.concat("DataFile")));
-    ArrayList<String> userData = new ArrayList<>();
-    String l = bfr.readLine();
-    while (l != null) {
-        userData.add(l);
-        l = bfr.readLine();
-    }
-    ArrayList<String> friends = new ArrayList<>();
-    ArrayList<String> blocked = new ArrayList<>();
-    ArrayList<Chat> chats = new ArrayList<>();
-    String[] friendString = userData.get(3).substring(7).split(",");
-    String[] blockedString = userData.get(4).substring(7).split(",");
-    if(!friendString[0].equals("Empty")) {
-        for(int i = 0; i < friendString.length; i ++) {
-            friends.add((friendString[i]));
-            chats.add(new Chat(emailInput.concat("_with_").concat(friendString[i])));
-            //create constructor for users based on info in file
-            //do the same for chats based on chat files
-            //this for loop will initialize a list of users and corresponding chats
+        //here is another example:
+        //edey@purdue.edu,DoubleDouble
+        //Zach
+        //Computer Science
+        //Friends:dngquan@purdue.edu,yash@purdue.edu,purduePete@purdue.edu...
+        //Blocked:Empty
+        //WhoCanMessage:friends
+        try {
+            BufferedReader bfr = new BufferedReader(new FileReader(emailInput.concat("DataFile")));
+            ArrayList<String> userData = new ArrayList<>();
+            String l = bfr.readLine();
+            while (l != null) {
+                userData.add(l);
+                l = bfr.readLine();
+            }
+            ArrayList<String> friends = new ArrayList<>();
+            ArrayList<String> blocked = new ArrayList<>();
+            ArrayList<Chat> chats = new ArrayList<>();
+            String[] friendString = userData.get(3).substring(7).split(",");
+            String[] blockedString = userData.get(4).substring(7).split(",");
+            if (!friendString[0].equals("Empty")) {
+                for (int i = 0; i < friendString.length; i++) {
+                    friends.add((friendString[i]));
+                    chats.add(new Chat(emailInput.concat("_with_").concat(friendString[i])));
+                    //create constructor for users based on info in file
+                    //do the same for chats based on chat files
+                    //this for loop will initialize a list of users and corresponding chats
+                }
+            }
+            if (!blockedString[0].equals("Empty")) {
+                for (int i = 0; i < blockedString.length; i++) {
+                    blocked.add((blockedString[i]));
+                    chats.add(new Chat(emailInput.concat("_with_").concat(blockedString[i])));
+                    //create constructor for users based on info in file
+                    //do the same for chats based on chat files
+                    //this for loop will initialize a list of users and corresponding chats
+                }
+            }
+            user = new User(userData.get(1), passwordInput, emailInput, userData.get(2), friends,
+                    blocked, chats, userData.get(5).split(":")[2].equals("friends"));
+            //Write new data to file either now or at end of program
+            //Show Options to exit(4), modify user information(3), search for users(2), or chat(1)
+            String choice = "";
+            do {
+                choice = reader.readLine();
+                if (choice.contains("[sending message command]")) {
+                    Chat chat = new Chat(user.getEmail() + "_with_" + choice.split(",,,")[1]);
+                    chat.addAMessage(choice.split(",,,")[choice.length() - 1], user);
+                    //allow user to pick who to talk to and allow them to send messages
+                } else if (choice.contains("[remove friend command]")) {
+                    user.removeFriend(choice.split("command]")[1]);
+                } else if (choice.contains("[unblock user command]")) {
+                    user.unblockUser(choice.split("command]")[1]);
+                } else if (choice.contains("[block user command]")) {
+                    user.blockUser(choice.split("command]")[1]);
+                }
+            } while (true);
+        } catch (Exception e) {
+
         }
-    }
-    if(!blockedString[0].equals("Empty")) {
-        for(int i = 0; i < blockedString.length; i ++) {
-            blocked.add((blockedString[i]));
-            chats.add(new Chat(emailInput.concat("_with_").concat(blockedString[i])));
-            //create constructor for users based on info in file
-            //do the same for chats based on chat files
-            //this for loop will initialize a list of users and corresponding chats
-        }
-    }
-    user = new User(userData.get(1), passwordInput, emailInput, userData.get(2), friends,
-            blocked, chats, userData.get(5).split(":")[2].equals("friends"));
-    //Write new data to file either now or at end of program
-    //Show Options to exit(4), modify user information(3), search for users(2), or chat(1)
-    int choice = 0;
-    do {
-        choice = scanner.nextInt();
-        if (choice == 1) {
-            user.canMessage(user);
-            //allow user to pick who to talk to and allow them to send messages
-        } else if (choice == 2) {
-            //search input is:
-            String searchInput = "";
-            data.usersNameSearch(user.getName());
-            data.usersMajorSearch(user.getMajor());
-            data.usersEmailSearch(user.getEmail());
-            user.addFriend(user);
-            user.removeFriend(user);
-            user.blockUser(user);
-            user.unblockUser(user);
-            //allow user to search for others based on major, name, or email and add, remove, or block people
-        } else if (choice == 3) {
-            //data.modifyUser(); needs to be void?
-            user.changeRestriction();
-            //allow user to modify name, message restriction, or password
-        }
-    } while (!(choice == 4));
-} catch(Exception e) {
-    
-}
 
     }
 
@@ -199,5 +197,45 @@ try {
 
     public void setWriter(PrintWriter writer) {
         this.writer = writer;
+    }
+
+    public void rewriteLoginDetails(ArrayList<String> emails, ArrayList<String> passwords) throws InvalidInputException {
+        String output = "";
+        for(int i = 0; i < emails.size(); i++) {
+            output = output.concat("\n" + emails.get(i) + "," + passwords.get(i));
+        }
+        if (!output.isEmpty()) {
+            output = output.substring(1);
+            try (PrintWriter out = new PrintWriter("LoginDetails.txt")) {
+                out.print(output);
+            } catch (Exception e) {
+                throw new InvalidInputException("Problem updating LoginDetails.txt");
+            }
+        }
+    }
+
+    public void createUserFile(String email, String password, String major, String name, String restriction)
+            throws InvalidInputException {
+        //jkaraki@purdue.edu,1234
+        //Jad
+        //Industrial Engineering
+        //Friends:dngquan@purdue.edu,yash@purdue.edu,purduePete@purdue.edu...
+        //Blocked:cs180@purdue.edu,csdep@purdue.edu...
+        //WhoCanMessage:all
+        String output = "";
+        output = output.concat("\n" + email);
+        output = output.concat("\n" + name);
+        output = output.concat("\n" + major);
+        output = output.concat("\nFriends:Empty");
+        output = output.concat("\nBlocked:Empty");
+        output = output.concat("\nWhoCanMessage:" + restriction);
+        if (!output.isEmpty()) {
+            output = output.substring(1);
+            try (PrintWriter out = new PrintWriter(email + ".txt")) {
+                out.print(output);
+            } catch (Exception e) {
+                throw new InvalidInputException("Problem creating user file");
+            }
+        }
     }
 }
