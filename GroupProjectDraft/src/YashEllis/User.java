ac55e51b78983f19eb030ed0d49f1c4e10df6ddc
yashv0ra <136860838+yashv0ra@users.noreply.github.com>
2024-04-24 21:06:36 -0400

    Update User.java

--- GroupProjectDraft/src/YashEllis/User.java
+++ GroupProjectDraft/src/YashEllis/User.java
@@ -115,143 +115,6 @@ public class User {
         }
     }
 
-    public void addFriend(User a) {
-        if (!friendsList.contains(a.getEmail()) && !blockedList.contains(a.getEmail())) {
-            friendsList.add(a.getEmail());
-        }
-    }
-
-    public void removeFriend(User a) {
-        friendsList.remove(a.getEmail());
-        blockedList.remove(a.getEmail());
-
-    }
-
-    public void blockUser(User a) {
-        if (!blockedList.contains(a.getEmail())) {
-            blockedList.add(a.getEmail());
-        }
-        friendsList.remove(a.getEmail());
-    }
-
-    public void unblockUser(User a) {
-        blockedList.remove(a.getEmail());
-    }
-
-//    public void checkChats() {
-//        //checks if chats are shared between this user and other users
-//        for (Chat c : chatList) {
-//            if (!((super.equals(c.getUser1()) || super.equals(c.getUser1()))
-//                    && !c.getUser2().equals(c.getUser2()))) {
-//                chatList.remove(c);
-//            }
-//        }
-//    }
-
-    public void changeRestriction() {
-        restrictMessage = !restrictMessage;
-    }
-
-    public boolean canMessage(User a) {
-        boolean friendCheck1 = false;
-        boolean friendCheck2 = false;
-        for (int i = 0; i < this.friendsList.size(); i++) {
-            if (this.friendsList.get(i).equals(a.getEmail())) {
-                friendCheck1 = true;
-                break;
-            }
-        }
-        for (int i = 0; i < a.friendsList.size(); i++) {
-            if (a.friendsList.get(i).equals(a.getEmail())) {
-                friendCheck2 = true;
-                break;
-            }
-        }
-        if (friendCheck2 && friendCheck1) {
-            return true;
-        }
-        // if the user restricts messages from people they aren't friends with
-        // and the other user is not your friend, then restrict
-        return !a.isRestrictMessage() || friendCheck2;
-    }
-
-    public boolean blocked(String email) {
-        for (int i = 0; i < this.blockedList.size(); i++) {
-            if (this.blockedList.get(i).equals(email)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public ArrayList<String> getFriendsList() {
-        return friendsList;
-    }
-
-    public ArrayList<String> getBlockedList() {
-        return blockedList;
-    }
-
-    public ArrayList<Chat> getChatList() {
-        return chatList;
-    }
-
-    public boolean isRestrictMessage() {
-        return restrictMessage;
-    }
-
-    public void setFriendsList(ArrayList<String> friendsList) {
-        this.friendsList = friendsList;
-    }
-
-    public void setBlockedList(ArrayList<String> blockedList) {
-        this.blockedList = blockedList;
-    }
-
-    public void setChatList(ArrayList<Chat> chatList) {
-        this.chatList = chatList;
-    }
-
-    public void setRestrictMessage(boolean restrictMessage) {
-        this.restrictMessage = restrictMessage;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getMajor() {
-        return major;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setMajor(String major) {
-        this.major = major;
-    }
-
-    public boolean compareTo(User user) {
-        return this.email.equals(user.email);
-    }
-
     public void rewriteUserFile() throws IOException {
         BufferedWriter bw = new BufferedWriter(new FileWriter(email, false));
         bw.write(email + "," + password);
@@ -281,5 +144,214 @@ public class User {
             bw.write("\nWhoCanMessage:all");
         }
         bw.close();
+    }
+    public boolean writeToFile() {
+        //edey@purdue.edu,DoubleDouble
+        //Zach
+        //Computer Science
+        //Friends:dngquan@purdue.edu,yash@purdue.edy,purduePete@purdue.edu...
+        //Blocked:Empty
+        //WhoCanMessage:friends
+        try {
+            PrintWriter pw = new PrintWriter(new FileWriter(new File(email)));
+            pw.write(email + "," + password + "\n");
+            pw.write(name + "\n");
+            pw.write(major + "\n");
+            pw.write("Friends:");
+            if (friendsList == null) {
+                pw.write("Empty");
+            } else {
+                for (int i = 0; i < friendsList.size(); i++) {
+                    pw.write(friendsList.get(i));
+                    if (i != friendsList.size() - 1) {
+                        pw.write(",");
+                    }
+                }
+            }
+            pw.write("\n" + "Blocked:");
+            if (blockedList == null) {
+                pw.write("Empty");
+            } else {
+                for (int i = 0; i < blockedList.size(); i++) {
+                    pw.write(blockedList.get(i));
+                    if (i != blockedList.size() - 1) {
+                        pw.write(",");
+                    }
+                }
+            }
+            if (restrictMessage) {
+                pw.write("\n WhoCanMessage:friends");
+            } else {
+                pw.write("\n WhoCanMessage:all");
+            }
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+    public void addFriend(User a) {
+        if (!friendsList.contains(a.getEmail()) && !blockedList.contains(a.getEmail())) {
+            friendsList.add(a.getEmail());
+        }
+        writeToFile();
+    }
+    public void addFriend(String email) {
+        if (!friendsList.contains(email) && !blockedList.contains(email)) {
+            friendsList.add(email);
+        }
+        writeToFile();
+    }
+
+    public void removeFriend(User a) {
+        friendsList.remove(a.getEmail());
+        blockedList.remove(a.getEmail());
+        writeToFile();
+    }
+    public void removeFriend(String email) {
+        friendsList.remove(email);
+        blockedList.remove(email);
+        writeToFile();
+    }
+
+    public void blockUser(User a) {
+        if (!blockedList.contains(a.getEmail())) {
+            blockedList.add(a.getEmail());
+        }
+        friendsList.remove(a.getEmail());
+        writeToFile();
+    }
+    public void blockUser(String email) {
+        if (!blockedList.contains(email)) {
+            blockedList.add(email);
+        }
+        friendsList.remove(email);
+        writeToFile();
+    }
+
+    public void unblockUser(User a) {
+        blockedList.remove(a.getEmail());
+        writeToFile();
+    }
+    public void unblockUser(String email) {
+        blockedList.remove(email);
+        writeToFile();
+    }
+
+//    public void checkChats() {
+//        //checks if chats are shared between this user and other users
+//        for (Chat c : chatList) {
+//            if (!((super.equals(c.getUser1()) || super.equals(c.getUser1()))
+//                    && !c.getUser2().equals(c.getUser2()))) {
+//                chatList.remove(c);
+//            }
+//        }
+//    }
+
+    public void changeRestriction() {
+        restrictMessage = !restrictMessage;
+        writeToFile();
+    }
+    public boolean canMessage(User a) {
+        boolean friendCheck1 = false;
+        boolean friendCheck2 = false;
+        for (int i = 0; i < this.friendsList.size(); i++) {
+            if (this.friendsList.get(i).equals(a.getEmail())) {
+                friendCheck1 = true;
+                break;
+            }
+        }
+        for (int i = 0; i < a.friendsList.size(); i++) {
+            if (a.friendsList.get(i).equals(a.getEmail())) {
+                friendCheck2 = true;
+                break;
+            }
+        }
+        if (friendCheck2 && friendCheck1) {
+            return true;
+        }
+        // if the user restricts messages from people they aren't friends with
+        // and the other user is not your friend, then restrict
+        return !a.isRestrictMessage() || friendCheck2;
+    }
+    public boolean blocked(String email) {
+        for (int i = 0; i < this.blockedList.size(); i++) {
+            if (this.blockedList.get(i).equals(email)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public ArrayList<String> getFriendsList() {
+        return friendsList;
+    }
+
+    public ArrayList<String> getBlockedList() {
+        return blockedList;
+    }
+
+    public ArrayList<Chat> getChatList() {
+        return chatList;
+    }
+
+    public boolean isRestrictMessage() {
+        return restrictMessage;
+    }
+
+    public void setFriendsList(ArrayList<String> friendsList) {
+        this.friendsList = friendsList;
+        writeToFile();
+    }
+
+    public void setBlockedList(ArrayList<String> blockedList) {
+        this.blockedList = blockedList;
+        writeToFile();
+    }
+
+    public void setChatList(ArrayList<Chat> chatList) {
+        this.chatList = chatList;
+        writeToFile();
+    }
+
+    public void setRestrictMessage(boolean restrictMessage) {
+        this.restrictMessage = restrictMessage;
+        writeToFile();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+        writeToFile();
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+        writeToFile();
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+        writeToFile();
+    }
+    public boolean compareTo(User user) {
+        return this.email.equals(user.email);
     }
 }
